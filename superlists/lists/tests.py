ae6110ac9aa8afa00f335c72b061610818734e0f
RodrigoFlBraz <rodrigoflbraz@gmail.com>
2023-05-04 22:43:57 -0300

    Apresentar Múltiplos Itens na Listagem

--- superlists/lists/tests.py
+++ superlists/lists/tests.py
@@ -6,6 +6,15 @@ from lists.views import home_page
 
 class HomePageTest(TestCase):
 
+    def test_displays_all_list_items(self):
+        Item.objects.create(text='itemey 1')
+        Item.objects.create(text='itemey 2')
+
+        response = self.client.get('/')
+
+        self.assertIn('itemey 1', response.content.decode())
+        self.assertIn('itemey 2', response.content.decode())
+
     
     def test_home_page_returns_correct_html(self):
         response = self.client.get('/')  
