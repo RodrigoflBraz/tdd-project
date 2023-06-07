8c377a08c9c3025f48bb92c6c3c7f6ba19850be9
RodrigoFlBraz <rodrigoflbraz@gmail.com>
2023-06-07 19:39:41 -0300

    Integrando o Bootstrap

--- superlists/functional_tests/tests.py
+++ superlists/functional_tests/tests.py
@@ -3,7 +3,6 @@ from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 import time
 from selenium.common.exceptions import WebDriverException
-from django.contrib.staticfiles.testing import StaticLiveServerTestCase
 
 
 MAX_WAIT = 5
